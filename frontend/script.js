02495fa06e2a433579c02a63f9ea2d7629ce8413
nnoranna <anagy.nora.anna@gmail.com>
2022-01-20 07:02:10 +0100

    Created data-handling branch, added new data

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,55 @@
-function loadEvent () {
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favouriteFood: "csirke/rizs"
+    },
+    {
+        name: "Krisztián Bui",
+        favouriteFood: "banán"
+    },
+    {
+        name: "Buczkó Balázs",
+        favouriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "András Varga",
+        favouriteFood: "brassói"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favouriteFood: "avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favouriteFood: "alma"
+    },
+    {
+        name: "Krisztián Pörneczi",
+        favouriteFood: "almás pite"
+    },
+]
+function loadEvent() {
     console.log("Az oldal betöltődött");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        `);
-        //elem.querySelector("h1").outerHTML = ""
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favouriteFood}</p>
+            </section>
+        `)
+        
     }
 }
 window.addEventListener("load", loadEvent);
